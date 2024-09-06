50497e30f7ebcc720f6f5159304eadff2964e0e3
AnnaTep <annspytonwork@gmail.com>
2024-09-06 12:51:01 +0500

    2 урок

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+kol_done_home_work = 12
+time_home_work = 1.5
+kurs = 'Python'
+one_home_work = time_home_work / kol_done_home_work
+print('Курс:',kurs,', всего задач:', kol_done_home_work,', затрачено часов:', time_home_work, ', среднее время выполнения:', one_home_work)
