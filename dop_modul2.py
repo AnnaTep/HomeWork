c50463bf4f6f557be905dfaac91557ba565203a0
AnnaTep <annspytonwork@gmail.com>
2024-09-17 13:37:57 +0500

    Доп.задание к модулю 2

---
+++ dop_modul2.py
@@ -0,0 +1,13 @@
+num = int(input('Number:'))
+l = []
+for i in range(num):
+    if i == 0:
+        continue
+    if i + num/2 < num:
+        p = i + (num - i)
+        if p == num:
+            l.append(i)
+            l.append(num-i)
+    else:
+        continue
+print(l)
