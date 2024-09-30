336ae6a561ac92dc85030acdd240a6d9ec0d4a38
AnnaTep <annspytonwork@gmail.com>
2024-09-30 15:43:19 +0500

    16 урок. Рекурсивное умножение цифр

---
+++ lesson16.py
@@ -0,0 +1,14 @@
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) == 1:
+        first = int(str_number[0])
+        return first
+    else:
+        if len(str_number) > 1:
+            first = first * get_multiplied_digits(int(str_number[1:]))
+    return first
+
+
+result = get_multiplied_digits(40203)
+print(result)
