d4facf8207f4521ae2f63ff500721b68c3cbb7e4
AnnaTep <annspytonwork@gmail.com>
2024-11-12 14:45:35 +0500

    7_5. Файлы в операционной системе

--- module_7_5.py
+++ module_7_5.py
@@ -5,9 +5,9 @@ directory = os.getcwd()
 print(directory)
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join('.venv/test_dir', file)
-        filetime = os.path.getmtime('')
+        filepath = os.path.join('.', file)
+        filetime = os.path.getmtime('.')
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize('')
+        filesize = os.path.getsize('.')
         parent_dir = os.path.dirname
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
