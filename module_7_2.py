6aafbe4fdf4858f3ee00620cb3f20a9738c428af
AnnaTep <annspytonwork@gmail.com>
2024-11-12 12:32:27 +0500

    7_2. Записать и запомнить

--- module_7_2.py
+++ module_7_2.py
@@ -2,15 +2,14 @@ def custom_write(file_name, strings):
     file = open(file_name, 'a', encoding='utf-8')
     strings_positions = 0
     strings_number = 0
-    #slovar = {}
+    slovar = dict()
     for i in strings:
         strings_number += 1
-        file.write(i+'\n')
-        a = (strings_number, strings_positions, i)
-        print(a)
         strings_positions = file.tell()
+        slovar[i] = (strings_number, strings_positions)
+        file.write(i+'\n')
     file.close()
-    #return slovar
+    return slovar
 
 info = [
     'Text for tell.',
@@ -20,5 +19,5 @@ info = [
     ]
 
 result = custom_write('test.txt', info)
-# for elem in result.items():
-#   print(elem)
+for elem in result.items():
+  print(elem)
