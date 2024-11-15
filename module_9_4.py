fef88d7960b6e8c332c01db421a9c3e88298e7fe
AnnaTep <annspytonwork@gmail.com>
2024-11-15 16:06:45 +0500

    9_4. Функциональное разнообразие

---
+++ module_9_4.py
@@ -0,0 +1,37 @@
+# Lambda-функция:
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+print(list(map(lambda x, y: x == y, first, second)))
+
+
+# Замыкание
+def get_advanced_writer(file_name):
+    file = open(file_name, 'a', encoding='utf-8')
+
+    def write_everything(*data_set):
+        for i in data_set:
+            file.write(str(i) + '\n')
+        file.close()
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+# Метод __call__
+from random import choice
+
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self, *args):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
